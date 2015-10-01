4121147291a1f1930a99cdce1accc4224dbb858a
Brent S.A. Cowgill <zardozcs@yahoo.com>
2015-10-01 01:33:04 +0100

    nocluster.js mock cluster and test plan

---
+++ test/nocluster.spec.js
@@ -0,0 +1,43 @@
+/* nocluster.spec.js */
+
+"use strict";
+
+var nocluster = require("../lib/nocluster"),
+	chai = require("chai"),
+	expect = chai.expect;
+
+function expectReadOnly (obj, prop) {
+	var now = obj[prop];
+
+	expect(function () {
+		obj[prop] = 42;
+	}).to.not.change(obj, prop);
+	expect(obj[prop]).to.be.equal(now);
+}
+
+describe("nocluster", function () {
+
+	it("should have isMock setting true", function () {
+		expect(nocluster.isMock).to.be.true;
+	});
+
+	it("should be readonly for isMock", function () {
+		expectReadOnly(nocluster, 'isMock');
+	});
+
+	it("should have isMaster readonly false", function () {
+		expect(nocluster.isMaster).to.be.false;
+		expectReadOnly(nocluster, 'isMaster');
+	});
+
+	it("should have isWorker readonly true", function () {
+		expect(nocluster.isWorker).to.be.true;
+		expectReadOnly(nocluster, 'isWorker');
+	});
+
+	it("should have worker id readonly 0", function () {
+		expect(nocluster.worker.id).to.be.true;
+		expectReadOnly(nocluster.worker, 'id');
+	});
+
+});
